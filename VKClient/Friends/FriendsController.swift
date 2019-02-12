2905a544d1c01f5fc11412cb4bf9676c4e8851c9
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-02-13 01:09:12 +0300

    no message

--- VKClient/Friends/FriendsController.swift
+++ VKClient/Friends/FriendsController.swift
@@ -125,7 +125,7 @@ class FriendsController: UITableViewController {
         cell.layer.add(animationTwo, forKey: nil)
     }
     
-    /*
+    /* 
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
