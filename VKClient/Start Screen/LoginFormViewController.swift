b714f390e98539d9f3726a8dc3aec51be527380e
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-02-19 22:10:50 +0300

    no message

--- VKClient/Start Screen/LoginFormViewController.swift
+++ VKClient/Start Screen/LoginFormViewController.swift
@@ -29,7 +29,7 @@ class LoginFormViewController: UIViewController {
             URLQueryItem(name: "client_id", value: "6849915"),
             URLQueryItem(name: "display", value: "mobile"),
             URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-            URLQueryItem(name: "scope", value: "270342"), //274438
+            URLQueryItem(name: "scope", value: "270342"), //262150
             URLQueryItem(name: "response_type", value: "token"),
             URLQueryItem(name: "v", value: "5.68")
         ]
