7704aeb737ec2b6dc6ff18b9ab0f436acd96e1f6
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-02-28 13:16:16 +0300

    перевел users на result. поправил весь friendsController

--- VKClient/Friends/FriendsController.swift
+++ VKClient/Friends/FriendsController.swift
@@ -12,13 +12,13 @@ import RealmSwift
 
 class FriendsController: UITableViewController {
     
-    var users = [User]()
-    var mySearchedUsers = [User]()
-    var allSearchedUsers = [User]()
+    var users: Results<User>?
+    var mySearchedUsers: Results<User>?
     
     let networkService = NetworkService()
     let dataService = DataService()
-    
+    var notificationToken: NotificationToken?
+
     let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
@@ -34,57 +34,75 @@ class FriendsController: UITableViewController {
             if let error = error {
                 print(error.localizedDescription)
                 return
-            } else if let users = users, let self = self {
-                self.users = users.filter {$0.lastName != ""}
-                
+            } else if let users = users?.filter({$0.lastName != ""}),
+                let self = self {
+
                 self.dataService.saveData(users)
-                
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
             }
         }
         
-        let config = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
-        let realm = try! Realm(configuration: config)
-        users = Array(realm.objects(User.self))
+        pairTableAndRealm()
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return isFiltering() ? firstLetters(in: mySearchedUsers).count : firstLetters(in: users).count
+        if isFiltering() {
+            guard let mySearchedUsers = mySearchedUsers else { return 0 }
+            return firstLetters(in: mySearchedUsers).count
+        } else {
+            guard let users = users else { return 0 }
+            return firstLetters(in: users).count
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return isFiltering() ? filterUsers(from: mySearchedUsers, in: section).count : filterUsers(from: users, in: section).count
+        if isFiltering() {
+            guard let mySearchedUsers = mySearchedUsers else { return 0 }
+            return filterUsers(from: mySearchedUsers, in: section).count
+        } else {
+            guard let users = users else { return 0 }
+            return filterUsers(from: users, in: section).count
+        }
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return isFiltering() ? firstLetters(in: mySearchedUsers)[section] : firstLetters(in: users)[section]
+        if isFiltering() {
+            guard let mySearchedUsers = mySearchedUsers else { return nil }
+            return firstLetters(in: mySearchedUsers)[section]
+        } else {
+            guard let users = users else { return nil }
+            return firstLetters(in: users)[section]
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsCell", for: indexPath) as! FriendsCell
         
         let friendAvatar = UIImageView()
-        var filteredFriends = [User]()
+        var filteredFriends: Results<User>
         
-        filteredFriends = isFiltering() ? filterUsers(from: mySearchedUsers, in: indexPath.section) : filterUsers(from: users, in: indexPath.section)
-        
-        cell.friendNameLabel.text = filteredFriends[indexPath.row].lastName + " " + filteredFriends[indexPath.row].firstName
-        
-        let border = UIView()
-        border.frame = cell.friendAvatar.bounds
-        border.layer.cornerRadius = cell.friendAvatar.bounds.height / 2
-        border.layer.masksToBounds = true
-        cell.friendAvatar.addSubview(border)
-        
-        if let avatar = filteredFriends[indexPath.row].avatar {
-            friendAvatar.kf.setImage(with: URL(string: avatar))
+        if isFiltering() {
+            guard let mySearchedUsers = mySearchedUsers else { return UITableViewCell() }
+            filteredFriends = filterUsers(from: mySearchedUsers, in: indexPath.section)
+        } else {
+            guard let users = users else { return UITableViewCell() }
+            filteredFriends = filterUsers(from: users, in: indexPath.section)
         }
         
-        friendAvatar.frame = border.bounds
-        border.addSubview(friendAvatar)
-        
+            cell.friendNameLabel.text = filteredFriends[indexPath.row].lastName + " " + filteredFriends[indexPath.row].firstName
+            
+            let border = UIView()
+            border.frame = cell.friendAvatar.bounds
+            border.layer.cornerRadius = cell.friendAvatar.bounds.height / 2
+            border.layer.masksToBounds = true
+            cell.friendAvatar.addSubview(border)
+            
+            if let avatar = filteredFriends[indexPath.row].avatar {
+                friendAvatar.kf.setImage(with: URL(string: avatar))
+            }
+            
+            friendAvatar.frame = border.bounds
+            border.addSubview(friendAvatar)
+//        }
         return cell
     }
     
@@ -109,27 +127,39 @@ class FriendsController: UITableViewController {
     }
     
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return isFiltering() ? firstLetters(in: mySearchedUsers) : firstLetters(in: users)
+        if isFiltering() {
+            guard let mySearchedUsers = mySearchedUsers else { return nil }
+            return firstLetters(in: mySearchedUsers)
+        } else {
+            guard let users = users else { return nil }
+            return firstLetters(in: users)
+        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowFoto" {
             let friendFotoController = segue.destination as! FriendFotoController
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                var filteredFriends = [User]()
-                filteredFriends = isFiltering() ? filterUsers(from: mySearchedUsers ,in: indexPath.section) : filterUsers(from: users ,in: indexPath.section)
+                var filteredFriends: Results<User>
+                if isFiltering() {
+                    guard let mySearchedUsers = mySearchedUsers else { return }
+                    filteredFriends = filterUsers(from: mySearchedUsers ,in: indexPath.section)
+                } else {
+                    guard let users = users else { return }
+                    filteredFriends = filterUsers(from: users ,in: indexPath.section)
+                }
                 friendFotoController.friendId = filteredFriends[indexPath.row].id
                 friendFotoController.friendName = filteredFriends[indexPath.row].lastName + " " + filteredFriends[indexPath.row].firstName
             }
         }
     }
     
-    func filterUsers (from users: [User], in section: Int) -> [User] {
+    func filterUsers (from users: Results<User>, in section: Int) -> Results<User> {
         let key = firstLetters(in: users)[section]
-        return users.filter { $0.lastName.first! == Character(key) }
+        return users.filter("lastName BEGINSWITH %@", key)
     }
  
-    func firstLetters (in users: [User]) -> [String] {
+    func firstLetters (in users: Results<User>) -> [String] {
         var firstLetters = [String]()
         for user in users {
             firstLetters.append(String(user.lastName.first!))
@@ -143,9 +173,7 @@ class FriendsController: UITableViewController {
     
     func filterContentForSearchText(_ searchText: String, scope: String = "All") {
         
-        mySearchedUsers = users.filter({( name ) -> Bool in
-            return name.lastName.lowercased().contains(searchText.lowercased()) || name.firstName.lowercased().contains(searchText.lowercased())
-        })
+        mySearchedUsers = users?.filter("lastName CONTAINS[cd] %@ OR firstName CONTAINS[cd] %@", searchText, searchText)
         
         tableView.reloadData()
     }
@@ -153,7 +181,22 @@ class FriendsController: UITableViewController {
     func isFiltering() -> Bool {
         return searchController.isActive && !searchBarIsEmpty()
     }
-    
+        
+    func pairTableAndRealm() {
+        guard let realm = try? Realm() else { return }
+        users = realm.objects(User.self)
+        notificationToken = users?.observe ({ [weak self] (changes: RealmCollectionChange) in
+            guard let tableView = self?.tableView else { return }
+            switch changes {
+            case .initial:
+                tableView.reloadData()
+            case .update:
+                tableView.reloadData()
+            case .error(let error):
+                fatalError("\(error)")
+            }
+        })
+    }
 }
 
 extension FriendsController: UISearchResultsUpdating {
