1c6019224c4429518e8216448f09126814117fda
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-03-12 06:48:43 +0300

    no message

--- VKClient/News/ConigureNewsCell.swift
+++ VKClient/News/ConigureNewsCell.swift
@@ -8,27 +8,27 @@
 
 import UIKit
 
-class ConigureNewsCell {
-    
-    func configure(_ news: News, cell: NewsCell) {
-        
-        let ownerPhoto = UIImageView()
-        
-        ownerPhoto.kf.setImage(with: URL(string: news.ownerPhoto))
-        cell.ownersName.text = Int(news.ownerId)! > 0 ? news.userName : news.groupName
-        cell.newsText.text = news.text
-        cell.likeButton.setTitle(String(news.likesCount), for: .normal)
-        cell.commentButton.setTitle(String(news.commentsCount), for: .normal)
-        cell.sharedButton.setTitle(String(news.repostsCount), for: .normal)
-        cell.watchedLabel.text = String(news.views)
-        cell.newsPhotoImage.kf.setImage(with: URL(string: news.newsPhoto))
-        
-        let border = UIView()
-        border.frame = cell.ownersPhoto.bounds
-        border.layer.cornerRadius = cell.ownersPhoto.bounds.height / 2
-        border.layer.masksToBounds = true
-        cell.ownersPhoto.addSubview(border)
-        ownerPhoto.frame = border.bounds
-        border.addSubview(ownerPhoto)
-    }
-}
+//class ConigureNewsCell {
+//
+//    func configure(_ news: News, cell: NewsCell) {
+//
+//        let ownerPhoto = UIImageView()
+//
+//        ownerPhoto.kf.setImage(with: URL(string: news.ownerPhoto))
+//        cell.ownersName.text = Int(news.ownerId)! > 0 ? news.userName : news.groupName
+//        cell.newsText.text = news.text
+//        cell.likeButton.setTitle(String(news.likesCount), for: .normal)
+//        cell.commentButton.setTitle(String(news.commentsCount), for: .normal)
+//        cell.sharedButton.setTitle(String(news.repostsCount), for: .normal)
+//        cell.watchedLabel.text = String(news.views)
+//        cell.newsPhotoImage.kf.setImage(with: URL(string: news.newsPhoto))
+//
+//        let border = UIView()
+//        border.frame = cell.ownersPhoto.bounds
+//        border.layer.cornerRadius = cell.ownersPhoto.bounds.height / 2
+//        border.layer.masksToBounds = true
+//        cell.ownersPhoto.addSubview(border)
+//        ownerPhoto.frame = border.bounds
+//        border.addSubview(ownerPhoto)
+//    }
+//}
