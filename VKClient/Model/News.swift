efb12c936d98efdc55c1610e33b16cf5a3749308
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-04-17 23:22:04 +0300

    вычислил aspectRatio для фото новостей

--- VKClient/Model/News.swift
+++ VKClient/Model/News.swift
@@ -17,6 +17,7 @@ class News: Object {
     @objc dynamic var repostOwnerId = 0
     @objc dynamic var text = ""
     @objc dynamic var newsPhoto = ""
+    @objc dynamic var newsPhotoAspectRatio = 0.0
     @objc dynamic var url = ""
     @objc dynamic var likes = 0
     @objc dynamic var isliked = 0
@@ -26,6 +27,8 @@ class News: Object {
     @objc dynamic var views = 0
     @objc dynamic var repostText = ""
     @objc dynamic var repostNewsPhoto = ""
+    @objc dynamic var repostNewsPhotoAspectRatio = 0.0
+    
     
     @objc dynamic var owner: Owner?
     @objc dynamic var repostOwner: Owner?
@@ -36,6 +39,7 @@ class News: Object {
         self.ownerId = json["source_id"].intValue > 0 ? json["source_id"].intValue : -json["source_id"].intValue
         self.text = json["text"].stringValue
         self.newsPhoto = json["attachments"][0]["photo"]["photo_604"].stringValue
+        self.newsPhotoAspectRatio = json["attachments"][0]["photo"]["height"].doubleValue != 0 ? json["attachments"][0]["photo"]["width"].doubleValue / json["attachments"][0]["photo"]["height"].doubleValue : 0.0
         self.url = json["attachments"][0]["link"]["url"].stringValue
         self.likes = json["likes"]["count"].intValue
         self.isliked = json["likes"]["user_likes"].intValue
@@ -45,6 +49,7 @@ class News: Object {
         self.views = json["views"]["count"].intValue
         self.repostText = json["copy_history"][0]["text"].stringValue
         self.repostNewsPhoto = json["copy_history"][0]["attachments"][0]["photo"]["photo_604"].stringValue
+        self.repostNewsPhotoAspectRatio = json["copy_history"][0]["attachments"][0]["photo"]["height"].doubleValue != 0 ? json["copy_history"][0]["attachments"][0]["photo"]["width"].doubleValue / json["copy_history"][0]["attachments"][0]["photo"]["height"].doubleValue : 0.0
         self.repostOwnerId = json["copy_history"][0]["owner_id"].intValue > 0 ? json["copy_history"][0]["owner_id"].intValue : -json["copy_history"][0]["owner_id"].intValue
     }
     
